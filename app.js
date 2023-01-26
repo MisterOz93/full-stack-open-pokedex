fc139c560fe738925ee0f357ec92e32a3ad78628
Michael Mroz <mroz15@hotmail.com>
2023-01-25 20:54:12 -0400

    fix health endpoint to properly send error response, specify node version in package.json

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const { execSync } = require('child_process')
+const { execSync } = require('child_process')
 const app = express()
 
 // Heroku dynamically sets a port
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res ) => {
+  // eslint-disable-next-line
+  console.log('updated')
+  try {
+    execSync('npm run custom-health-check')
+  } catch (e) {
+    return res.status(400).send('error')
+  }
   res.send('Ok')
 })
 
