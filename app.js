fcb090de47b1a76b59a0ead0f97ebb044a8839f5
Michael Mroz <mroz15@hotmail.com>
2023-01-26 20:21:03 -0400

    bypass pr bug by doing custom healthcheck instead of check deploy if pipeline triggered by synchronize

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res ) => {
-  res.send('Ok')
+  return res.status(200).send('Ok')
 })
 
 app.listen(PORT, () => {
